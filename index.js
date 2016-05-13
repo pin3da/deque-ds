cd5a95fe1fe5e32cf3ef89a20357ac35114b9c8e
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-05-13 10:56:11 -0500

    using camel case notation. Bump version :rocket:

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
 module.exports = Deque;
 
-function Deque() {
+function Deque () {
   if (!(this instanceof Deque)) return new Deque();
   this._data = {};
   this._back = 0;
   this._front = 0;
 }
 
-Deque.prototype.push_back = function(value) {
+Deque.prototype.pushBack = function (value) {
   this._data[this._back++] = value;
-}
+};
 
-Deque.prototype.push_front = function(value) {
+Deque.prototype.pushFront = function (value) {
   this._front--;
   this._data[this._front] = value;
-}
+};
 
-Deque.prototype.front = function() {
-  if (this._front < this._back)
-    return this._data[this._front];
-  return null;
-}
-
-Deque.prototype.pop_front= function() {
+Deque.prototype.front = function () {
   if (this._front < this._back) {
-    val = this._data[this._front];
+    return this._data[this._front];
+  }
+  return null;
+};
+
+Deque.prototype.popFront = function () {
+  if (this._front < this._back) {
+    var val = this._data[this._front];
     delete this._data[this._front];
     this._front++;
     return val;
   }
   return null;
-}
+};
 
-
-Deque.prototype.back = function() {
-  if (this._front < this._back)
-    return this._data[this._back - 1];
-  return null;
-}
-
-Deque.prototype.pop_back = function() {
+Deque.prototype.back = function () {
   if (this._front < this._back) {
-    val = this._data[this._back - 1];
+    return this._data[this._back - 1];
+  }
+  return null;
+};
+
+Deque.prototype.popBack = function () {
+  if (this._front < this._back) {
+    var val = this._data[this._back - 1];
     delete this._data[this._back - 1];
     this._back--;
     return val;
   }
   return null;
-}
+};
 
-Deque.prototype.at = function(index) {
-  if (index > this._back - this._front)
+Deque.prototype.at = function (index) {
+  if (index > this._back - this._front) {
     return null;
+  }
   return this._data[this._front + index];
-}
+};
 
-Deque.prototype.size = function() {
+Deque.prototype.size = function () {
   return this._back - this._front;
-}
+};
--- test.js
+++ test.js
@@ -1,35 +1,39 @@
 var tap = require('tap');
 var deque = require('./index')();
 
-tap.test('testing deque', function(t) {
+tap.test('testing deque', function (t) {
+  'use strict';
   t.equals(deque.size(), 0);
 
-  for (var i = 0; i < 20; ++i)
-    deque.push_back(i);
+  for (let i = 0; i < 20; ++i) {
+    deque.pushBack(i);
+  }
 
   t.equals(deque.size(), 20);
 
-  for (var i = 0; i < 20; ++i)
-    t.equals(deque.pop_front(), i);
+  for (let i = 0; i < 20; ++i) {
+    t.equals(deque.popFront(), i);
+  }
 
   t.equals(deque.size(), 0);
   t.equals(deque.front(), null);
   t.equals(deque.back(), null);
-  t.equals(deque.pop_front(), null);
-  t.equals(deque.pop_back(), null);
+  t.equals(deque.popFront(), null);
+  t.equals(deque.popBack(), null);
   t.equals(deque.at(50), null);
 
-  for (var i = 0; i < 20; ++i)
-    deque.push_front(i);
-
+  for (let i = 0; i < 20; ++i) {
+    deque.pushFront(i);
+  }
 
   t.equals(deque.size(), 20);
   t.equals(deque.at(3), 16);
   t.equals(deque.front(), 19, 'testing front');
   t.equals(deque.back(), 0, 'testing back');
 
-  for (var i = 0; i < 5; ++i)
-    t.equals(deque.pop_back(), i);
+  for (let i = 0; i < 5; ++i) {
+    t.equals(deque.popBack(), i);
+  }
 
   t.end();
 });
